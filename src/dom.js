e54fd6234f750c2f9fd9b30621ac8de32941034e
Randolf Resurreccion <randolfr@gmail.com>
2020-11-24 02:16:28 -0500

    dom objects to be used on our app.js

---
+++ src/dom.js
@@ -0,0 +1,20 @@
+export const query = {
+    dice: document.querySelectorAll('.dice'),
+    score: document.querySelectorAll('.player-score'),
+    current: document.querySelectorAll('.player-current-score'),
+    player1Panel: document.querySelector('.player-0-panel'),
+    player2Panel: document.querySelector('.player-1-panel'),
+    targetScore: document.querySelector('.target-score'),
+}
+
+export const btn = {
+    new: document.querySelector('.btn-new'),
+    roll: document.querySelector('.btn-roll'),
+    hold: document.querySelector('.btn-hold'),
+}
+
+export const dice = {
+    one: document.querySelector('#dice-1'),
+    two: document.querySelector('#dice-2'),
+    both: document.querySelectorAll('.dice'),
+}
